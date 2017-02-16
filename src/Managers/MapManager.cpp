90cead9ea1f0114ec1c9b0862eed705c64f5e4f7
Lisa <robbie@uni-koblenz.de>
2017-02-16 20:52:38 +0100

    map_manager without laser scans

--- src/Managers/MapManager.cpp
+++ src/Managers/MapManager.cpp
@@ -21,18 +21,18 @@ MapManager::MapManager(ros::NodeHandle *nh) {
         m_MapVisibility[m_laser_layers[i]] = true;
     }
 
-    try {
-        m_TransformListener.waitForTransform("/base_link", "/laser",
-                                             ros::Time(0), ros::Duration(3));
-        m_TransformListener.lookupTransform("/base_link", "/laser",
-                                            ros::Time(0), m_sick_transform);
-        m_TransformListener.lookupTransform("/base_link", "/hokuyo_front",
-                                            ros::Time(0), m_hokuyo_transform);
-        m_got_transform = true;
-    } catch (tf::TransformException ex) {
-        ROS_ERROR("%s", ex.what());
-        m_got_transform = false;
-    }
+    //try {
+        //m_TransformListener.waitForTransform("/base_link", "/laser",
+                                             //ros::Time(0), ros::Duration(3));
+        //m_TransformListener.lookupTransform("/base_link", "/laser",
+                                            //ros::Time(0), m_sick_transform);
+        //m_TransformListener.lookupTransform("/base_link", "/hokuyo_front",
+                                            //ros::Time(0), m_hokuyo_transform);
+        //m_got_transform = true;
+    //} catch (tf::TransformException ex) {
+        //ROS_ERROR("%s", ex.what());
+        //m_got_transform = false;
+    //}
     geometry_msgs::PoseStamped pose;
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
@@ -108,116 +108,116 @@ void MapManager::sendMergedMap() {
     }
     // bake Lasers
 
-    try {
-        int data;
-        if (m_got_transform) {
-            for (int j = 0; j < m_laser_layers.size(); j++) {
-                if (m_laserLayers.find(m_laser_layers[j]) !=
-                        m_laserLayers.end() &&
-                    m_MapVisibility[m_laser_layers[j]]) {
-                    if (m_laser_layers[j] ==
-                        homer_mapnav_msgs::MapLayers::SICK_LAYER) {
-                        data = homer_mapnav_msgs::ModifyMap::BLOCKED;
-                    } else if (m_laser_layers[j] ==
-                               homer_mapnav_msgs::MapLayers::HOKUYO_BACK) {
-                        data = homer_mapnav_msgs::ModifyMap::HOKUYO;
-                    } else if (m_laser_layers[j] ==
-                               homer_mapnav_msgs::MapLayers::HOKUYO_FRONT) {
-                        data = homer_mapnav_msgs::ModifyMap::HOKUYO;
-                    }
-                    tf::StampedTransform pose_transform;
-                    m_TransformListener.waitForTransform(
-                        "/map", "/base_link",
-                        m_laserLayers[m_laser_layers[j]]->header.stamp,
-                        ros::Duration(0.09));
-                    m_TransformListener.lookupTransform(
-                        "/map", "/base_link",
-                        m_laserLayers[m_laser_layers[j]]->header.stamp,
-                        pose_transform);
+    //try {
+        //int data;
+        //if (m_got_transform) {
+            //for (int j = 0; j < m_laser_layers.size(); j++) {
+                //if (m_laserLayers.find(m_laser_layers[j]) !=
+                        //m_laserLayers.end() &&
+                    //m_MapVisibility[m_laser_layers[j]]) {
+                    //if (m_laser_layers[j] ==
+                        //homer_mapnav_msgs::MapLayers::SICK_LAYER) {
+                        //data = homer_mapnav_msgs::ModifyMap::BLOCKED;
+                    //} else if (m_laser_layers[j] ==
+                               //homer_mapnav_msgs::MapLayers::HOKUYO_BACK) {
+                        //data = homer_mapnav_msgs::ModifyMap::HOKUYO;
+                    //} else if (m_laser_layers[j] ==
+                               //homer_mapnav_msgs::MapLayers::HOKUYO_FRONT) {
+                        //data = homer_mapnav_msgs::ModifyMap::HOKUYO;
+                    //}
+                    //tf::StampedTransform pose_transform;
+                    //m_TransformListener.waitForTransform(
+                        //"/map", "/base_link",
+                        //m_laserLayers[m_laser_layers[j]]->header.stamp,
+                        //ros::Duration(0.09));
+                    //m_TransformListener.lookupTransform(
+                        //"/map", "/base_link",
+                        //m_laserLayers[m_laser_layers[j]]->header.stamp,
+                        //pose_transform);
 
-                    //	pose_transform.setTransform(Transform);
+                    ////	pose_transform.setTransform(Transform);
 
-                    for (int i = 0;
-                         i < m_laserLayers[m_laser_layers[j]]->ranges.size();
-                         i++) {
-                        if (m_laserLayers[m_laser_layers[j]]->ranges[i] <
-                                m_laserLayers[m_laser_layers[j]]->range_max &&
-                            m_laserLayers[m_laser_layers[j]]->ranges[i] >
-                                m_laserLayers[m_laser_layers[j]]->range_min) {
-                            float alpha =
-                                m_laserLayers[m_laser_layers[j]]->angle_min +
-                                i *
-                                    m_laserLayers[m_laser_layers[j]]
-                                        ->angle_increment;
-                            geometry_msgs::Point point;
-                            tf::Vector3 pin;
-                            tf::Vector3 pout;
-                            pin.setX(
-                                cos(alpha) *
-                                m_laserLayers[m_laser_layers[j]]->ranges[i]);
-                            pin.setY(
-                                sin(alpha) *
-                                m_laserLayers[m_laser_layers[j]]->ranges[i]);
-                            pin.setZ(0);
+                    //for (int i = 0;
+                         //i < m_laserLayers[m_laser_layers[j]]->ranges.size();
+                         //i++) {
+                        //if (m_laserLayers[m_laser_layers[j]]->ranges[i] <
+                                //m_laserLayers[m_laser_layers[j]]->range_max &&
+                            //m_laserLayers[m_laser_layers[j]]->ranges[i] >
+                                //m_laserLayers[m_laser_layers[j]]->range_min) {
+                            //float alpha =
+                                //m_laserLayers[m_laser_layers[j]]->angle_min +
+                                //i *
+                                    //m_laserLayers[m_laser_layers[j]]
+                                        //->angle_increment;
+                            //geometry_msgs::Point point;
+                            //tf::Vector3 pin;
+                            //tf::Vector3 pout;
+                            //pin.setX(
+                                //cos(alpha) *
+                                //m_laserLayers[m_laser_layers[j]]->ranges[i]);
+                            //pin.setY(
+                                //sin(alpha) *
+                                //m_laserLayers[m_laser_layers[j]]->ranges[i]);
+                            //pin.setZ(0);
 
-                            if (m_laser_layers[j] ==
-                                homer_mapnav_msgs::MapLayers::SICK_LAYER) {
-                                pout = pose_transform * m_sick_transform * pin;
-                            } else if (m_laser_layers[j] ==
-                                       homer_mapnav_msgs::MapLayers::
-                                           HOKUYO_FRONT) {
-                                pout =
-                                    pose_transform * m_hokuyo_transform * pin;
-                            }
-                            point.x = pout.x();
-                            point.y = pout.y();
-                            point.z = 0;
+                            //if (m_laser_layers[j] ==
+                                //homer_mapnav_msgs::MapLayers::SICK_LAYER) {
+                                //pout = pose_transform * m_sick_transform * pin;
+                            //} else if (m_laser_layers[j] ==
+                                       //homer_mapnav_msgs::MapLayers::
+                                           //HOKUYO_FRONT) {
+                                //pout =
+                                    //pose_transform * m_hokuyo_transform * pin;
+                            //}
+                            //point.x = pout.x();
+                            //point.y = pout.y();
+                            //point.z = 0;
 
-                            Eigen::Vector2i map_point = map_tools::toMapCoords(
-                                point,
-                                m_MapLayers
-                                    [homer_mapnav_msgs::MapLayers::SLAM_LAYER]
-                                        ->info.origin,
-                                m_MapLayers
-                                    [homer_mapnav_msgs::MapLayers::SLAM_LAYER]
-                                        ->info.resolution);
-                            k = map_point.y() *
-                                    m_MapLayers[homer_mapnav_msgs::MapLayers::
-                                                    SLAM_LAYER]
-                                        ->info.width +
-                                map_point.x();
-                            if (k < 0 ||
-                                k > m_MapLayers[homer_mapnav_msgs::MapLayers::
-                                                    SLAM_LAYER]
-                                        ->data.size()) {
-                                continue;
-                            } else {
-                                mergedMap.data[k] = data;
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            try {
-                if (m_TransformListener.waitForTransform("/base_link", "/laser",
-                                                         ros::Time(0),
-                                                         ros::Duration(0.1))) {
-                    m_TransformListener.lookupTransform(
-                        "/base_link", "/laser", ros::Time(0), m_sick_transform);
-                    m_TransformListener.lookupTransform(
-                        "/base_link", "/hokuyo_front", ros::Time(0),
-                        m_hokuyo_transform);
-                    m_got_transform = true;
-                }
-            } catch (tf::TransformException ex) {
-                ROS_ERROR("%s", ex.what());
-                m_got_transform = false;
-            }
-        }
-    } catch (tf::TransformException ex) {
-        ROS_ERROR_STREAM(ex.what());
-    }
+                            //Eigen::Vector2i map_point = map_tools::toMapCoords(
+                                //point,
+                                //m_MapLayers
+                                    //[homer_mapnav_msgs::MapLayers::SLAM_LAYER]
+                                        //->info.origin,
+                                //m_MapLayers
+                                    //[homer_mapnav_msgs::MapLayers::SLAM_LAYER]
+                                        //->info.resolution);
+                            //k = map_point.y() *
+                                    //m_MapLayers[homer_mapnav_msgs::MapLayers::
+                                                    //SLAM_LAYER]
+                                        //->info.width +
+                                //map_point.x();
+                            //if (k < 0 ||
+                                //k > m_MapLayers[homer_mapnav_msgs::MapLayers::
+                                                    //SLAM_LAYER]
+                                        //->data.size()) {
+                                //continue;
+                            //} else {
+                                //mergedMap.data[k] = data;
+                            //}
+                        //}
+                    //}
+                //}
+            //}
+        //} else {
+            //try {
+                //if (m_TransformListener.waitForTransform("/base_link", "/laser",
+                                                         //ros::Time(0),
+                                                         //ros::Duration(0.1))) {
+                    //m_TransformListener.lookupTransform(
+                        //"/base_link", "/laser", ros::Time(0), m_sick_transform);
+                    //m_TransformListener.lookupTransform(
+                        //"/base_link", "/hokuyo_front", ros::Time(0),
+                        //m_hokuyo_transform);
+                    //m_got_transform = true;
+                //}
+            //} catch (tf::TransformException ex) {
+                //ROS_ERROR("%s", ex.what());
+                //m_got_transform = false;
+            //}
+        //}
+    //} catch (tf::TransformException ex) {
+        //ROS_ERROR_STREAM(ex.what());
+    //}
     mergedMap.header.stamp = ros::Time::now();
     mergedMap.header.frame_id = "map";
 
